bd8ee126cb0608fbaaf4e0aa35b4835356a8f698
rharmonson <richard.harmonson@gmail.com>
2018-07-27 07:20:53 -0700

    add button01

--- oop.py
+++ oop.py
@@ -5,33 +5,27 @@ import tkinter as tk
 class PathFrame(tk.Frame):
     def __init__(self, root):
         super().__init__()
-        self['height'] = 100
-        self['width'] = 800
         self['bg'] = 'lightblue'
 
 
 class FilesFrame(tk.Frame):
     def __init__(self, root):
         super().__init__()
-        self['height'] = 200
-        self['width'] = 400
         self['bg'] = 'grey'
 
 
 class TestFrame(tk.Frame):
     def __init__(self, root):
         super().__init__()
-        self['height'] = 200
-        self['width'] = 400
         self['bg'] = 'white'
 
 
 class RegexFrame(tk.Frame):
     def __init__(self, root):
         super().__init__()
-        self['height'] = 100
-        self['width'] = 800
         self['bg'] = 'lightblue'
+        self.button01 = tk.Button(self.tk.frame, text='Button 01')
+        self.button01.grid()
 
 
 root = tk.Tk()
@@ -43,9 +37,9 @@ frame_middle_left = FilesFrame(root)
 frame_middle_right = TestFrame(root)
 frame_bottom = RegexFrame(root)
 
-frame_top.grid(row=0, column=0)
-frame_middle_left.grid(row=1, column=0)
-frame_middle_right.grid(row=1, column=1)
-frame_bottom.grid(row=2, column=0)
+frame_top.grid(row=0, column=0, rowspan=1, columnspan=6, sticky='ew')
+frame_middle_left.grid(row=1, column=0, rowspan=1, columnspan=3, sticky='ewns')
+frame_middle_right.grid(row=1, column=3, rowspan=1, columnspan=3, sticky='ewns')
+frame_bottom.grid(row=2, column=0, rowspan=1, columnspan=6, sticky='ew')
 
 root.mainloop()
